3f467ef004d7f21a5acdbf2c98a5788ffcec88a4
pawan <pawannagar1996@gmail.com>
2022-09-02 19:35:22 +0530

    add operators

---
+++ operators/operators.js
@@ -0,0 +1,59 @@
+
+
+
+//operators
+
+//1. ( = ) assigment
+
+const a = 10;
+
+console.log(a);
+
+
+// Arithmetic Operators (+, -, *, /, %, **)
+
+
+let x = 20;
+
+let y = 20;
+
+
+console.log(x + y); // 40
+console.log(x - y); // 0
+console.log(x * y); //400
+console.log(x / y); // 1
+console.log(x % y); // 0
+console.log(x ** y); // 1.048576e+26
+console.log(2 ** 3); // 8
+
+
+
+
+console.log('10' + '10') ; // its a string ('x'+'y'= xy) but bakiyo m y string ignore karega('2'*'2'= 4)
+
+// Subtraction (+=, -=, *=, /=, %=, **=)
+
+
+console.log(x += y); 
+console.log(x -= y);
+console.log(x *= y); 
+console.log(x /= y);
+console.log(x %=y); 
+//console.log('10' += '10') ;
+
+
+
+
+
+
+// comprariser(
+// == kya y braber h (mix string and numberArray. this is not compeyer)
+// === kya y braber h (y compeyer krta h value ki string h ya number)
+// >=
+// <=>
+// <
+// != not
+
+// )
+
+
