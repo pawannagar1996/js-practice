89760f62600ae7cfd20e29f7c5231b17563154c9
pawan <pawannagar1996@gmail.com>
2022-09-07 10:28:48 +0530

    add funtion _name

---
+++ condition/condition.js
@@ -0,0 +1,57 @@
+
+
+
+//conditions
+
+// 1. if else
+
+
+
+const a = "tydd" ;
+
+console.log(a);
+
+
+if(typeof a == 'number'){console.log('u type number')}
+
+else{console.log('u type string')};
+
+
+
+console.log('==============================================================================')
+
+
+//law ke hisab s 3 saal ya 3 s kamm saal ka baccha school nahi ja sakta
+// 5 saal ke bache 3 class, 
+// 6 or 7 saal ke bache 4 class,
+// 8 or 9 saal k bacche 5 class,
+
+
+
+const age = 4;
+
+if(typeof age == 'number'){
+
+    if(age >= 0 || age <=3 ){console.log('abe badha to hone de')}
+   else if(age < 5){console.log('tu abhi padh nahi sakta')}
+
+
+    else if(age < 6){console.log('3rd class in')}
+    else if(age < 8){console.log('4rd class in')}
+    else if(age < 10){console.log('5rd class in')}
+
+    else{console.log('hamre yaha class nahi h')}
+
+    
+}
+else{console.log('its not a number')};
+
+
+
+
+console.log('==============================================================================');
+
+
+
+
+
---
+++ functions/functions.js
@@ -0,0 +1,21 @@
+
+
+
+let age = 15 ;
+
+
+function insertAge (age)
+{
+ if(age <=10){console.log("you are inter");}
+ else{console.log("only allowed less then 11", age);}
+
+};
+
+
+
+insertAge(9);
+insertAge(12);
+
+
+
+
