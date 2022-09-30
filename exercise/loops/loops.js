9dd1707c9da9aa1fdfae0f0ddab389c52b3fd3d2
pawan <pawannagar1996@gmail.com>
2022-09-30 19:10:34 +0530

    practice loops

--- exercise/loops/loops.js
+++ exercise/loops/loops.js
@@ -1,32 +1,24 @@
 
 let Num = 0
-
-
 while (Num <= 5) {
-
     if (Num <= 5) {
-
     }
     console.log(Num)
     Num++
 }
-
 console.log('=========================')
 
 
 let outerIndex = 0;
 
 while (outerIndex <= 5) {
-
     let str = '';
     innerIndex = 0;
-
     while (innerIndex <= outerIndex) {
         str += '* '
         innerIndex++
     }
     console.log(str)
-
     outerIndex++
 }
 
@@ -37,14 +29,11 @@ console.log('=========================')
 // outerIndex = 0;
 
 // while (outerIndex <= 5) {
-
 //     let str1 = '';
 //     let str = '*****';
 //     innerIndex = 0;
 //     innerIndex2 = 0;
-
 //     while (innerIndex <= outerIndex) {
-
 //         str1 = str.length - innerIndex2
 //         str.splice(str1)
 //         break
@@ -52,7 +41,6 @@ console.log('=========================')
 //         innerIndex2++
 //     }
 //     console.log(str1)
-
 //     outerIndex += 1
 // }
 
@@ -69,20 +57,36 @@ console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
 // *
 
 let outerIndex3 = 0;
-
 while (outerIndex3 <= 5) {
     let str2 = ''
     let innerIndex = 5;
-
-
     while (outerIndex3 < innerIndex) {
         str2 += '* '
         innerIndex--
-
     }
     console.log(str2)
     outerIndex3++
+}
 
+console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+
+//    *
+//   **
+//  ***
+// ****
+//*****
+
+let outerIndex6 = 0;
+while (outerIndex6 < 5) {
+    let str6 = ''
+    let innerIndex = 0;
+    while (innerIndex <= 4) {
+        if (innerIndex <= 3) { str6 += '#' }
+        else if (innerIndex === 4) { str6 += '*' }
+        innerIndex++
+    }
+    console.log(str6)
+    outerIndex6++
 }
 
 console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
@@ -97,17 +101,13 @@ let outerIndex4 = 0;
 while (outerIndex4 <= 5) {
     let str3 = '';
     let innerIndex = 0;
-
     while (innerIndex <= outerIndex4) {
-
         if (innerIndex % 2 === 0) {
             str3 += '*'
         } else { str3 += '$' }
-
         innerIndex++
     }
     console.log(str3)
-
     outerIndex4++
 }
 
@@ -124,26 +124,17 @@ console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
 
 let add = 3 % 3
 console.log(add)
-
-
 let outerIndex5 = 0;
 
 while (outerIndex5 <= 6) {
     let str4 = '';
     let innerIndex = 0;
-
     while (innerIndex <= outerIndex5) {
-
         if (innerIndex % 3 === 0) {
-
             str4 += '$'
         } else { str4 += '*' }
-
-
-
         innerIndex++
     }
-
     console.log(str4)
     outerIndex5++
 }
