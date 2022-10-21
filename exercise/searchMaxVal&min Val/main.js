0557d1130fcbc6df6e736ec19e3ff09f47018c3d
pawan <pawannagar1996@gmail.com>
2022-10-21 19:03:31 +0530

    max and min values

---
+++ exercise/searchMaxVal&min Val/main.js
@@ -0,0 +1,42 @@
+
+
+
+
+const values = [3,12, 5, 32, 2, 36, 6]
+
+let maxVal = 0
+let secMaxVal = 0
+let minVal= values[0];
+let secMinVal = 0
+let len = values.length
+let index = 0
+
+while(index<len){
+    let numVal = values[index]
+
+    if(maxVal<numVal){
+
+        secMaxVal=maxVal
+        maxVal=numVal
+       
+    }
+    else if (minVal>numVal){
+        secMinVal=minVal
+        minVal=numVal
+      
+    }
+
+    else{
+
+        if(secMaxVal<numVal){
+            secMaxVal=numVal
+        }
+    }
+
+    index++
+}
+
+console.log('maximum value = ', maxVal)
+console.log('second maximum value = ', secMaxVal)
+console.log('minimum value = ', minVal)
+console.log('second minimum value = ', secMinVal)
